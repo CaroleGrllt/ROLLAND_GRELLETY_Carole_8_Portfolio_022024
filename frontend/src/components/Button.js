f977bcddbcb41663074a3ecd84f93ae9d616dd33
Carole ROLLAND GRELLETY <carole.grellety@gmail.com>
2024-03-07 09:48:19 +0100

    Style cards

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Button({link, text}) {
 return (
-    <div>
+    <div className="btn">
         <Link to={link}>
             <span>{text}</span>
         </Link>
